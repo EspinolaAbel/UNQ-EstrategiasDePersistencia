24e2aa1aebc291e5c05b453951eb7f663236c4d7
EspinolaAbel <espinola.abel.e@gmail.com>
2016-08-28 14:50:39 -0300

    Pequeña act. los directorios...

--- src/main/java/ar/edu/unq/epers/bichomon/backend/dao/impl/JDBCBichoDAO.java
+++ src/main/java/ar/edu/unq/epers/bichomon/backend/dao/impl/JDBCBichoDAO.java
@@ -1,10 +1,10 @@
-package ar.edu.unq.epers.bichomon.backend.DAO.JDBC.bicho;
+package ar.edu.unq.epers.bichomon.backend.dao.impl;
 
 /**
  * Esta clase tiene como función manejar la persistencia de la clase Bicho en mi BD mySql,
  * pesistiendo con JDBC
  * 
  * @author Abel Espínola*/
-public class BichoDAO {
+public class JDBCBichoDAO {
 	
 }
