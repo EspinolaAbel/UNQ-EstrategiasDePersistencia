c7691b4c1c21b6b2dac19e088b02eeb4904448b9
EspinolaAbel <espinola.abel.e@gmail.com>
2016-08-28 18:03:01 -0300

    Update BD-Bichomon.sql

--- src/main/resources/BD-Bichomon/BD-Bichomon.sql
+++ src/main/resources/BD-Bichomon/BD-Bichomon.sql
@@ -1,12 +1,13 @@
 CREATE DATABASE Bichomon;
 USE Bichomon;
-CREATE TABLE Especie(nombre VARCHAR(30), tipo VARCHAR(20), altura INT,peso FLOAT, cantidad_de_bichos INT, PRIMARY KEY(nombre));
+CREATE TABLE Especie(nombre VARCHAR(30), tipo VARCHAR(20), altura INT,peso FLOAT, cantidad_de_bichos INT, url_foto VARCHAR(100), PRIMARY KEY(nombre));
 CREATE TABLE Bicho(nombre VARCHAR(30), nombre_especie VARCHAR(30), PRIMARY KEY(nombre), CONSTRAINT fk_Bicho FOREIGN KEY (nombre_especie) REFERENCES Especie(nombre));
 
 
 #NOTA
 #----
-##Cambié algunos nombre de columnas porque estaban en cammel case, ahora están con guión bajo.
+##Cambié algunos nombre de columnas porque estaban en cammel case, ahora están con guión bajo. También agregué la columna url_foto
+##a la tabla Especie.
 ##Si ya cargaste esta BD antes del cambio, tenés que cambiarle los 
 ##nombres a esas columnas corriendo lo que está abajo (descomentar primero).
 
@@ -15,3 +16,6 @@ CREATE TABLE Bicho(nombre VARCHAR(30), nombre_especie VARCHAR(30), PRIMARY KEY(n
 #alter table Bicho drop foreign key fk_Bicho;
 #alter table Bicho change column nombreEspecie nombre_especie Varchar(20) ;
 #alter table Bicho add constraint fk_Bicho foreign key (nombre_especie) references Especie(nombre);
+
+#alter table Especie add column url_foto varchar(100);
+
