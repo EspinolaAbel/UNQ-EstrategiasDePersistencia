ee011ce7433763c82ce53134b5b48e046361e2c4
= <=>
2016-09-19 08:57:10 -0300

    clase busqueda , interface Factores creadas, agrego al entrenador captura y descarte de bichos, actualize uml

---
+++ src/main/java/ar/edu/unq/epers/bichomon/backend/model/busqueda/IFactores.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ar.edu.unq.epers.bichomon.backend.model.busqueda;
+
+import ar.edu.unq.epers.bichomon.backend.model.entrenador.Entrenador;
+import ar.edu.unq.epers.bichomon.backend.model.lugar.Lugar;
+
+/**interface para calcular la probabilidadd de que la busqueda  sea exitosa
+ * @author Peter
+ *
+ */
+public interface IFactores {
+
+	public double factorTiempo (Entrenador entrenador);
+	public double factorPoblacion (Lugar pueblo);
+	public double factorNivel (Entrenador entreneador);
+	
+	
+}
