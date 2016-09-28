88a797935473871847c2049ccbeb0d2d42c5d825
javier <peter_ja2003@yahoo.com.ar>
2016-09-27 21:21:01 -0300

    hibernateBichoDAO

--- src/main/java/ar/edu/unq/epers/bichomon/backend/dao/BichoDAO.java
+++ src/main/java/ar/edu/unq/epers/bichomon/backend/dao/BichoDAO.java
@@ -1,5 +1,11 @@
 package ar.edu.unq.epers.bichomon.backend.dao;
 
+import ar.edu.unq.epers.bichomon.backend.model.Bicho;
+
 public interface BichoDAO {
+	
+	public void saveBicho(Bicho bicho);
+	public Bicho getBicho(int id );
+	
 
 }
